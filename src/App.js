3bf24d88f781e31c25eff3d2cc48bacb69f26172
Syed Raza Ali <razaali26296@gmail.com>
2024-06-30 14:13:06 +0530

    started random color generator

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Accordion from "./components/accordian";
+import RandomColor from "./components/random-color";
 
 function App() {
   return (
     <div className="App">
       {/* Accordian */}
       <Accordion />
+      {/* Random Color */}
+      <RandomColor />
     </div>
   );
 }
---
+++ src/components/random-color/index.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+export default function RandomColor() {
+  const [typeOfColor, setTypeOfColor] = useState("hex");
+  const [color, setColor] = useState("#000000");
+  function handleCreateRandomHexColor() {}
+  function handleCreateRandomRgbColor() {}
+  return (
+    <div style={{ backgroundColor: color, width: "100vw", height: "100vh" }}>
+      <button onClick={() => setTypeOfColor("hex")}>Create HEX Color</button>
+      <button onClick={() => setTypeOfColor("rgb")}>Create RGB Color</button>
+      <button
+        onClick={
+          typeOfColor === "hex"
+            ? handleCreateRandomHexColor
+            : handleCreateRandomRgbColor
+        }
+      >
+        Generate Random Color
+      </button>
+    </div>
+  );
+}
