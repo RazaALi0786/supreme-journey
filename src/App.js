9aad334f2f7fa2a135b012b1890b9293218303f1
Syed Raza Ali <razaali26296@gmail.com>
2024-07-06 19:13:30 +0530

    made image slider

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Accordion from "./components/accordian";
+import ImageSlider from "./components/image-slider";
 import RandomColor from "./components/random-color";
 import StarRating from "./components/star rating";
 
@@ -12,6 +13,12 @@ function App() {
       <RandomColor />
       {/* Star Rating */}
       <StarRating noOfStars={10} />
+      {/* Image Slider */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </div>
   );
 }
